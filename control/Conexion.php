0d7597b292f05d4593f57534c85faf8800d98ce3
McAs <mcas@live.com.co>
2024-03-04 22:49:13 -0500

    Eliminar archivos CSS y vistas de usuario, actualizar conexión a la base de datos y genera crud de usuarios

--- control/Conexion.php
+++ control/Conexion.php
@@ -1,45 +1,28 @@
 <?php
-
 class ConexionBD {
     private $servidor;
     private $usuario;
     private $password;
     private $pdo;
 
-    public function __construct($servidor, $usuario, $password) {
+    public function __construct($servidor, $usuario, $password){
         $this->servidor = $servidor;
         $this->usuario = $usuario;
         $this->password = $password;
     }
 
-    public function conectar() {
-        try {
+    public function conectar(){
+        try{
             $this->pdo = new PDO($this->servidor, $this->usuario, $this->password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
             //echo "Conectado...";
-        } catch (PDOException $e) {
+        } catch(PDOException $e){
             echo "Conexión Fallida: " . $e->getMessage();
         }
         return $this->pdo;
     }
 
-    public function desconectar() {
+    public function desconectar(){
         $this->pdo = null;
     }
 }
-?>
-
-<?php
-/*
-$servidor="mysql:dbname=bdindicadores1;host=127.0.0.1";
-$usuario="root";
-$password="";
-
-try{
-    $pdo = new PDO($servidor,$usuario,$password,array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
-    //echo "Conectado...";
-
-}catch(PDOException $e){
-    echo "Conexión Fallida: ".$e->getMessage();
-}
-*/
 ?>
--- control/ControlUsuario.php
+++ control/ControlUsuario.php
@@ -8,7 +8,7 @@ class ControlUsuario{
     function guardar(){
         $email = $this->objUsuario->getEmail();
         $contrasena = $this->objUsuario->getContrasena();
-        $conexionBD = new ConexionBD("mysql:dbname=bdindicadores1;host=127.0.0.1", "root", "");
+        $conexionBD = new ConexionBD("mysql:host=localhost;dbname=bdindicadores1;","root","");
         $pdo = $conexionBD->conectar();
         $comandoSql = $pdo->prepare("INSERT INTO usuario (email, contrasena) VALUES ('$email', '$contrasena')");
         $comandoSql->execute();
@@ -16,28 +16,26 @@ class ControlUsuario{
     }
     function modificar($newContra){
         $email = $this->objUsuario->getEmail();
-        $conexionBD = new ConexionBD("mysql:dbname=bdindicadores1;host=127.0.0.1", "root", "");
+        $conexionBD = new ConexionBD("mysql:host=localhost;dbname=bdindicadores1;","root","");
         $pdo = $conexionBD->conectar();
         $comandoSql = $pdo->prepare("UPDATE usuario SET contrasena = '$newContra' WHERE email = '$email'");
         $comandoSql->execute();
         $conexionBD->desconectar();
     }
-    function borrar(){
-        $email = $this->objUsuario->getEmail();
-        $conexionBD = new ConexionBD("mysql:dbname=bdindicadores1;host=127.0.0.1", "root", "");
+    function borrar($email){
+        $conexionBD = new ConexionBD("mysql:host=localhost;dbname=bdindicadores1;","root","");
         $pdo = $conexionBD->conectar();
-        $comandoSql = $pdo->prepare("DELETE FROM usuario WHERE 'email' = '$email'");
+        $comandoSql = $pdo->prepare("DELETE FROM usuario WHERE email = '$email'");
         $comandoSql->execute();
         $conexionBD->desconectar();
     }
     function consultar(){
-        //$email = $this->objUsuario->getEmail();
-        $objControlConexion = new ControlConexion();
-        $comandoSql = "SELECT email from 'usuario'";
-        $objControlConexion->abrirBd('localhost', 'root', '', 'bdindicadores1', 3306);
-        $result = $objControlConexion->ejecutarComandoSql($comandoSql);
-        $objControlConexion->cerrarBd();
-        return $result;
+        $conexionBD = new ConexionBD("mysql:host=localhost;dbname=bdindicadores1;","root","");
+        $pdo = $conexionBD->conectar();
+        $comandoSql = $pdo->prepare("SELECT * FROM usuario");
+        $comandoSql->execute();
+        $conexionBD->desconectar();
+        return $comandoSql->fetchAll(PDO::FETCH_ASSOC);
     }
 }
 ?>
