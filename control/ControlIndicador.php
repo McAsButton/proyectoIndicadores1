72f1c6879d44fd949f916dcf70917c9ceaa10a02
McAs <mcas@live.com.co>
2024-03-18 19:41:07 -0500

    Nuevo control indicador

---
+++ control/ControlIndicador.php
@@ -0,0 +1,37 @@
+<?php
+include '../Conexion.php'; // cspell:disable-line <- desabilita el corrector ortografico para esta linea
+include '../modelo/Indicador.php';
+
+class ControlIndicador{
+    var $objIndicador;
+
+    function __construct($objIndicador){
+        $this->objIndicador=$objIndicador;
+    }
+    
+    function consultar(){
+        $id=$this->objIndicador->getId();
+        $codigo=$this->objIndicador->getCodigo();
+        $nombre=$this->objIndicador->getNombre();
+        $objetivo=$this->objIndicador->getObjetivo();
+        $alcance=$this->objIndicador->getAlcance();
+        $formula=$this->objIndicador->getFormula();
+        $fkidtipoindicador=$this->objIndicador->getFkidtipoindicador();
+        $fkidunidadmedicion=$this->objIndicador->getFkidunidadmedicion();
+        $meta=$this->objIndicador->getMeta();
+        $fkidsentido=$this->objIndicador->getFkidsentido();
+        $fkidfrecuencia=$this->objIndicador->getFkidfrecuencia();
+        $fkidarticulo=$this->objIndicador->getFkidarticulo();
+        $fkidliteral=$this->objIndicador->getFkidliteral();
+        $fkidnumeral=$this->objIndicador->getFkidnumeral();
+        $fkidparagrafo=$this->objIndicador->getFkidparagrafo();
+
+        $conexionBD = new ConexionBD("mysql:host=localhost;dbname=bdindicadores1;","root","");
+        $pdo = $conexionBD->conectar();
+        $comandoSql = $pdo->prepare("SELECT * FROM usuario");
+        $comandoSql->execute();
+        $conexionBD->desconectar();
+        return $comandoSql->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+?>
