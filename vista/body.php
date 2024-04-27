c8b29e9dcb9dad63b60bfa9690a383f670d94c94
McAsButton <mcas@live.com.co>
2024-04-27 07:37:11 -0500

    COmmit

--- vista/body.php
+++ vista/body.php
@@ -16,16 +16,16 @@
                     if (isset($_SESSION['email'])) {
                         echo '<li class="dropdown"><a href="#"><span>Vistas</span> <i class="bi bi-chevron-down"></i></a>';
                         echo '<ul>';
-                        echo '<li><a href="vistaFuente.php">Vista Fuente</a></li>';
-                        echo '<li><a href="vistaRepresenVisual.php">Vista Represen Visual</a></li>';
-                        echo '<li><a href="vistaRol.php">Vista Rol</a></li>';
-                        echo '<li><a href="vistaSeccion.php">Vista Sección</a></li>';
-                        echo '<li><a href="vistaSentido.php">Vista Sentido</a></li>';
-                        echo '<li><a href="vistaSubSeccion.php">Vista Sub Sección</a></li>';
-                        echo '<li><a href="vistaTipoActor.php">Vista Tipo Actor</a></li>';
-                        echo '<li><a href="vistaTipoIndicador.php">Vista Tipo Indicador</a></li>';
-                        echo '<li><a href="vistaUnidadMedicion.php">Vista Unidad Medición</a></li>';
-                        echo '<li><a href="vistaUsuario.php">Vista Usuario</a></li>';
+                        echo '<li><a href="vistaFuente.php">Fuente</a></li>';
+                        echo '<li><a href="vistaRepresenVisual.php">Represen Visual</a></li>';
+                        echo '<li><a href="vistaRol.php">Rol</a></li>';
+                        echo '<li><a href="vistaSeccion.php">Sección</a></li>';
+                        echo '<li><a href="vistaSentido.php">Sentido</a></li>';
+                        echo '<li><a href="vistaSubSeccion.php">Sub Sección</a></li>';
+                        echo '<li><a href="vistaTipoActor.php">Tipo Actor</a></li>';
+                        echo '<li><a href="vistaTipoIndicador.php">Tipo Indicador</a></li>';
+                        echo '<li><a href="vistaUnidadMedicion.php">Unidad Medición</a></li>';
+                        echo '<li><a href="vistaUsuario.php">Usuario</a></li>';
                         echo '</ul>';
                         echo '</li>';
                     }
